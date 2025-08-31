6c61004dced7502d1d4983b78797bf58204ef64a
snehsatasiya-creator <snehsatasiya@gmail.com>
2025-08-31 10:36:10 +0530

    Create 4th Prog

---
+++ 4. Divisible by 7 or Not.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if(n%7==0)
+        printf("Divisible by 7");
+    else
+        printf("Not divisible by 7");
+    return 0;
+}
